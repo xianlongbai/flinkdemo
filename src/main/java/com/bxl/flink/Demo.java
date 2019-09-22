9a6e1a972405c50ffba807abf6839f3c689eefba
xianlong bai <xianlong.bai@tendcloud.com>
2019-09-22 12:32:08 +0800

    Initial commit

---
+++ src/main/java/com/bxl/flink/Demo.java
@@ -0,0 +1,12 @@
+package com.bxl.flink;
+
+/**
+ * Created by root on 2019/9/21.
+ */
+public class Demo {
+
+    public static void main(String[] args){
+        System.out.println(11111);
+    }
+
+}
---
+++ src/main/scala/com/bxl/flink/Test.scala
@@ -0,0 +1,16 @@
+package com.bxl.flink
+
+/**
+  * Created by root on 2019/9/21.
+  */
+class Test {
+
+}
+object Test{
+
+  def main(args: Array[String]): Unit = {
+
+    println(11111)
+
+  }
+}
